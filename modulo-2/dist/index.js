a701820e3945fd877357852278920db243974c14
Edrick Crousset Peña <edrickcp19@gmail.com>
2026-05-12 11:10:11 +0200

    Proyecto limpio: módulos configurados y archivos pesados ignorados

--- modulo-2/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const domain_js_1 = require("./domain.js");
-const miEstado = {
-    tipo: "ACTIVA",
-    asignaturas: ["TypeScript", "React", "Node"]
-};
-console.log((0, domain_js_1.generarReporte)(miEstado));
